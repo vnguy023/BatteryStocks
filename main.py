b530797caecb18eccdd8e81bd2e4d1bee74887f9
Vincent Nguyen <vincentnguyen88@hotmail.com>
2021-01-08 20:01:06 -0800

    cache update logic is now in a function. cache stays fresh for 5 minutes

--- main.py
+++ main.py
@@ -4,22 +4,17 @@ import stockinfo.stockinfo as si
 from datetime import datetime
 import utils.utils as utils
 
-def main(portfolioJson):
+MINUTES_TO_SECONDS = 60
+
+def main(portfolioJson, updateCacheAfterMinutes):
     currentTime = datetime.now()
     currentEpoch = int(currentTime.timestamp())
+    lastEpoch = utils.getLastUpdateEpochTime()
     
     print( "Current Time:\t", currentTime.strftime("%Y-%m-%d %H:%M") )
-    print( "Current Epoch:\t", currentEpoch)
-
-    lastEpoch = utils.getLastUpdateEpochTime()
-
-    print( "Last Epoch:\t", lastEpoch)
 
     print("***********************************")
-
-    UPDATE_DATA = False
-
-    if UPDATE_DATA:
+    if isCacheStale(currentEpoch, lastEpoch, updateCacheAfterMinutes * MINUTES_TO_SECONDS):
         print("===Updating Data===")
         ticker = 'TSLA'
 
@@ -29,11 +24,22 @@ def main(portfolioJson):
     else:
         print("===Using Cached Data===")
 
+def isCacheStale(currentEpoch, lastEpoch, cacheFreshSeconds):
+    print( "Current Epoch:\t", currentEpoch)
+    print( "Last Epoch:\t", lastEpoch)
+
+    if currentEpoch < lastEpoch:
+        return False
+    elif (currentEpoch - lastEpoch) < cacheFreshSeconds:
+        return False
+
+    return True
+
 numArgs = len(sys.argv) - 1
 
 # This parses arugments to make sure it's valid
 if numArgs == 1:
-    main(sys.argv[1])
+    main(sys.argv[1], 5)
 else:
     print("Improper Usage: Format <portfolio>")
     print("Ex: py3 main.py sample.portfolio")
