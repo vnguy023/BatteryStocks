6c22d6bdd2c8422e53fd95e3c8220a8b9d7c84b1
Vincent Nguyen <vincentnguyen88@hotmail.com>
2021-01-11 17:08:50 -0800

    show gain changes for ledgers, trimmed out some unneeded fields

--- portfolio/ledger.py
+++ portfolio/ledger.py
@@ -21,25 +21,67 @@ class Ledger:
     def getCostBasis(self):
         return self.shares * self.pricePaid
 
+    def getDayChange(self):
+        return self.getPrice() - self.getYesterdayClosingPrice()
+
+    def getLastMonthClosingPrice(self):
+        return TickerCache.getInstance().getLastMonthClosingPrice(self.ticker)
+
+    def getLastWeekClosingPrice(self):
+        return TickerCache.getInstance().getLastWeekClosingPrice(self.ticker)
+
     def getMarketValue(self):
         return self.getPrice() * self.shares
 
+    def getMarketValueLastMonth(self):
+        return self.shares * self.getLastMonthClosingPrice()
+
+    def getMarketValueLastWeek(self):
+        return self.shares * self.getLastWeekClosingPrice()
+
+    def getMarketValueYesterday(self):
+        return self.shares * self.getYesterdayClosingPrice()
+
+    def getMonthChange(self):
+        return self.getPrice() - self.getLastMonthClosingPrice()
+
     def getPrice(self):
         return TickerCache.getInstance().getCurrentPrice(self.ticker)
+        
+    def getWeekChange(self):
+        return self.getPrice() - self.getLastWeekClosingPrice()
 
     def getYesterdayClosingPrice(self):
         return TickerCache.getInstance().getYesterdayClosingPrice(self.ticker)
 
+    def getTotalGain(self):
+        return self.getMarketValue() - self.getCostBasis()
+
+    def getDayGain(self):
+        return self.getMarketValue() - self.getMarketValueYesterday()
+
+    def getWeekGain(self):
+        return self.getMarketValue() - self.getMarketValueLastWeek()
+
+    def getMonthGain(self):
+        return self.getMarketValue() - self.getMarketValueLastMonth()
+
     @classmethod
     def printHeader(cls, prependStr:str):
         defaultColor = TextColor.CGREY + TextColor.CBOLD + TextColor.CITALIC
-        pricePaidStr = utils.getStrValueOutput("{:^14}".format("PricePaid"), defaultColor, defaultColor)
+        
         costBasisStr = utils.getStrValueOutput("{:^14}".format("CostBasis"), defaultColor, defaultColor)
         marketValueStr = utils.getStrValueOutput("{:^14}".format("MarketValue"), defaultColor, defaultColor)
         sharesStr = utils.getStrValueOutput("{:^10}".format("Shares"), defaultColor, defaultColor)
-        priceStr = utils.getStrValueOutput("{:^14}".format("Current Price"), defaultColor, defaultColor)
+        pricePaidStr = utils.getStrValueOutput("{:^14}".format("PricePaid"), defaultColor, defaultColor)
+
+        totalGainStr = utils.getStrValueOutput("{:^14}".format("TotalGain"), defaultColor, defaultColor)
+        dayGainStr = utils.getStrValueOutput("{:^14}".format("DayGain"), defaultColor, defaultColor)
+        weekGainStr = utils.getStrValueOutput("{:^14}".format("WeekGain"), defaultColor, defaultColor)
+        monthGainStr = utils.getStrValueOutput("{:^14}".format("MonthGain"), defaultColor, defaultColor)
         
-        output = prependStr + priceStr + " " + marketValueStr + " " + sharesStr + " " + pricePaidStr + " " + costBasisStr
+        output = prependStr + sharesStr + " " + pricePaidStr + " " + costBasisStr + " " + marketValueStr + " "
+        output = output + totalGainStr + " " + dayGainStr + " " + weekGainStr + " " + monthGainStr + " "
         print(output)
 
     def print(self, prependStr:str):
@@ -49,15 +91,32 @@ class Ledger:
         if self.getMarketValue() > self.getCostBasis():
             marketValueColor = TextColor.CGREEN + TextColor.CDIM + TextColor.CITALIC
 
-        priceValueColor = TextColor.CRED + TextColor.CDIM + TextColor.CITALIC
-        if self.getYesterdayClosingPrice() <= self.getPrice():
-            priceValueColor = TextColor.CGREEN + TextColor.CDIM + TextColor.CITALIC
+        totalGainColor = TextColor.CRED + TextColor.CDIM + TextColor.CITALIC
+        if self.getTotalGain() > 0:
+            totalGainColor = TextColor.CGREEN + TextColor.CDIM + TextColor.CITALIC
+
+        dayGainColor = TextColor.CRED + TextColor.CDIM + TextColor.CITALIC
+        if self.getDayGain() > 0:
+            dayGainColor = TextColor.CGREEN + TextColor.CDIM + TextColor.CITALIC
+
+        weekGainColor = TextColor.CRED + TextColor.CDIM + TextColor.CITALIC
+        if self.getWeekGain() > 0:
+            weekGainColor = TextColor.CGREEN + TextColor.CDIM + TextColor.CITALIC
+
+        monthGainColor = TextColor.CRED + TextColor.CDIM + TextColor.CITALIC
+        if self.getMonthGain() > 0:
+            monthGainColor = TextColor.CGREEN + TextColor.CDIM + TextColor.CITALIC
 
         costBasisStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getCostBasis())), defaultColor, defaultColor)
         marketValueStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getMarketValue())), defaultColor, marketValueColor)
         sharesStr = utils.getStrValueOutput("{:>10}".format(utils.getDecimalStr(self.shares)), defaultColor, defaultColor)
-        priceStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getPrice())), defaultColor, priceValueColor)
         pricePaidStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.pricePaid)), defaultColor, defaultColor)
         
-        output = prependStr + priceStr + " " + marketValueStr + " " + sharesStr + " " + pricePaidStr + " " + costBasisStr
+        totalGainStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getTotalGain())), defaultColor, totalGainColor) 
+        dayGainStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getDayGain())), defaultColor, dayGainColor)
+        weekGainStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getWeekGain())), defaultColor, weekGainColor)
+        monthGainStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getMonthGain())), defaultColor, monthGainColor)
+
+        output = prependStr + sharesStr + " " + pricePaidStr + " " + costBasisStr + " " + marketValueStr + " "
+        output = output + totalGainStr + " " + dayGainStr + " " + weekGainStr + " " + monthGainStr + " "
         print(output)
--- portfolio/stock.py
+++ portfolio/stock.py
@@ -23,17 +23,41 @@ class Stock:
             result = result + ledger.getCostBasis()
         return result
 
+    def getDayChange(self):
+        return self.getPrice() - self.getYesterdayClosingPrice()
+
+    def getLastMonthClosingPrice(self):
+        return TickerCache.getInstance().getLastMonthClosingPrice(self.ticker)
+
+    def getLastWeekClosingPrice(self):
+        return TickerCache.getInstance().getLastWeekClosingPrice(self.ticker)
+
+    def getMarketValue(self):
+        return self.getPrice() * self.getShares()
+
+    def getMonthChange(self):
+        return self.getPrice() - self.getLastMonthClosingPrice()
+
+    def getMarketValueLastMonth(self):
+        return self.shares * self.getLastMonthClosingPrice()
+
+    def getMarketValueLastWeek(self):
+        return self.shares * self.getLastWeekClosingPrice()
+
+    def getMarketValueYesterday(self):
+        return self.shares * self.getYesterdayClosingPrice()
+
+    def getPrice(self):
+        return TickerCache.getInstance().getCurrentPrice(self.ticker)
+
     def getShares(self):
         result = 0.00
         for ledger in self.ledgers:
             result = result + ledger.shares
         return result
 
-    def getMarketValue(self):
-        return self.getPrice() * self.getShares()
-
-    def getPrice(self):
-        return TickerCache.getInstance().getCurrentPrice(self.ticker)
+    def getWeekChange(self):
+        return self.getPrice() - self.getLastWeekClosingPrice()
 
     def getYesterdayClosingPrice(self):
         return TickerCache.getInstance().getYesterdayClosingPrice(self.ticker)
@@ -43,12 +67,15 @@ class Stock:
         defaultColor = TextColor.CWHITE + TextColor.CBOLD
         avgPricePaidStr = utils.getStrValueOutput("{:^14}".format("avgPricePaid"), defaultColor, defaultColor)
         costBasisStr = utils.getStrValueOutput("{:^14}".format("CostBasis"), defaultColor, defaultColor)
+        dayChangeStr = utils.getStrValueOutput("{:^10}".format("DayChange"), defaultColor, defaultColor)
+        weekChangeStr = utils.getStrValueOutput("{:^10}".format("WeekChange"), defaultColor, defaultColor)
+        monthChangeStr = utils.getStrValueOutput("{:^10}".format("MonthChange"), defaultColor, defaultColor)
         marketValueStr = utils.getStrValueOutput("{:^14}".format("MarketValue"), defaultColor, defaultColor)
-        priceStr = utils.getStrValueOutput("{:^14}".format("Current Price"), defaultColor, defaultColor)
+        priceStr = utils.getStrValueOutput("{:^14}".format("CurrentPrice"), defaultColor, defaultColor)
         sharesStr = utils.getStrValueOutput("{:^10}".format("Shares"), defaultColor, defaultColor)
         tickerStr = utils.getStrValueOutput("{:^6}".format("Ticker"), defaultColor, defaultColor)
         
-        output = prependStr + tickerStr + " " + priceStr + " " + marketValueStr + " " + sharesStr + " " + avgPricePaidStr + " " + costBasisStr
+        output = prependStr + tickerStr + " " + priceStr + " " + dayChangeStr + " " + marketValueStr + " " + sharesStr + " " + avgPricePaidStr + " " + costBasisStr
         print(output)
 
     def print(self, prependStr:str):
@@ -58,16 +85,27 @@ class Stock:
         if self.getMarketValue() > self.getCostBasis():
             marketValueColor = TextColor.CGREEN
 
-        priceValueColor = TextColor.CRED
+        dayChangeColor = TextColor.CRED
         if self.getYesterdayClosingPrice() <= self.getPrice():
-            priceValueColor = TextColor.CGREEN
+            dayChangeColor = TextColor.CGREEN
+
+        weekChangeColor = TextColor.CRED
+        if self.getYesterdayClosingPrice() <= self.getPrice():
+            weekChangeColor = TextColor.CGREEN
+            
+        monthChangeColor = TextColor.CRED
+        if self.getYesterdayClosingPrice() <= self.getPrice():
+            monthChangeColor = TextColor.CGREEN
 
         avgPricePaidStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getAvgPricePaid())), defaultColor, defaultColor)
         costBasisStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getCostBasis())), defaultColor, defaultColor)
+        dayChangeStr = utils.getStrValueOutput("$" + "{:>9}".format(utils.getDecimalStr(self.getDayChange())), defaultColor, dayChangeColor)
+        weekChangeStr = utils.getStrValueOutput("$" + "{:>9}".format(utils.getDecimalStr(self.getWeekChange())), defaultColor, weekChangeColor)
+        monthChangeStr = utils.getStrValueOutput("$" + "{:>9}".format(utils.getDecimalStr(self.getMonthChange())), defaultColor, monthChangeColor)
         marketValueStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getMarketValue())), defaultColor, marketValueColor)
-        priceStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getPrice())), defaultColor, priceValueColor)
+        priceStr = utils.getStrValueOutput("$" + "{:>13}".format(utils.getDecimalStr(self.getPrice())), defaultColor, defaultColor)
         sharesStr = utils.getStrValueOutput("{:>10}".format(utils.getDecimalStr(self.getShares())), defaultColor, defaultColor)
         tickerStr = utils.getStrValueOutput("{:6}".format(self.ticker.symbol), defaultColor, defaultColor)
         
-        output = prependStr + tickerStr + " " + priceStr + " " + marketValueStr + " " + sharesStr + " " + avgPricePaidStr + " " + costBasisStr
+        output = prependStr + tickerStr + " " + priceStr + " " + dayChangeStr + " " + marketValueStr + " " + sharesStr + " " + avgPricePaidStr + " " + costBasisStr
         print(output)
